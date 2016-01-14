c2e22435cd24629358df26ed63bc528d7460e8b8
Brian Balsamo <balsamo@uchicago.edu>
2016-01-14 11:14:10 -0600

    A few handy functions for navigation with keylists and the start of validation.

--- uchicagoldrrecords/record.py
+++ uchicagoldrrecords/record.py
@@ -2,6 +2,8 @@ from copy import deepcopy
 from os.path import isabs
 from csv import DictReader
 from ast import literal_eval
+from re import compile as re_compile
+from re import match
 
 from uchicagoldr.batch import Directory
 from uchicagoldrconfig.LDRConfiguration import LDRConfiguration
@@ -10,6 +12,9 @@ config = LDRConfiguration().read_config_data()
 
 
 class Record(object):
+
+    allowed_types = ['str', 'dict', 'bool', 'list', 'int', 'float']
+
     def __init__(self, config_override_location=None):
         self.directory = None
         self.dictionary = {}
@@ -59,13 +64,21 @@ class Record(object):
             for entry in config:
                 assert(isinstance(entry, dict))
                 reqs = ["Field Name", "Value Type", "Default Value",
-                        "Validation", "Required for Accession",
+                        "Validation", "Required For Accession",
                         "Required For Completion"
                         ]
                 for req in reqs:
                     assert(req in entry)
+                assert(entry['Value Type'] in self.allowed_types)
+                assert(entry['Required For Accession'] in ['True', 'False'])
+                assert(entry['Required For Completion'] in ['True', 'False'])
+                try:
+                    re_compile(entry['Validation'])
+                except:
+                    raise AssertionError
             return True
-        except:
+        except Exception as e:
+            print(e)
             return False
 
     def read_config_file(self):
@@ -86,7 +99,7 @@ class Record(object):
         for row in self.get_config():
             key = row['Field Name']
             dataType = row['Value Type']
-            if dataType not in ['str', 'dict', 'bool', 'list', 'int', 'float']:
+            if dataType not in self.allowed_types:
                 raise ValueError("The following row has an invalid " +
                                  "Value Type!\n{}".format(row))
             else:
@@ -109,6 +122,29 @@ class Record(object):
                 new_dict[key] = value
         self.set_dictionary(new_dict)
 
+    def validate(self):
+        assert(self.validate_config())
+        problemFields = []
+        recordDict = self.get_dictionary()
+        for row in self.get_config():
+            if row['Validation'] != "":
+                key = row['Field Name']
+                value = recordDict[key]
+                if type(value) != str:
+                    value = str(value)
+                regex = row['Validation']
+                print(key + ": " + regex + " " + str(type(regex)) + " " +str(type(value)))
+                if not match(regex, value):
+                    print("Failed")
+                    problemFields.append(row['Field Name'])
+                else:
+                    print("Good")
+        if len(problemFields > 0):
+            return (False, problemFields)
+        else:
+            return (True, problemFields)
+
+
     def meld(self, b, a=None, path=None, collisions=None):
         if collisions is None:
             collisions = []
@@ -120,10 +156,10 @@ class Record(object):
         for key in b:
             if key in a:
                 if isinstance(a[key], dict) and isinstance(b[key], dict):
-                    a[key], collisions = self.merge(b[key],
-                                                    a[key],
-                                                    path + [str(key)],
-                                                    collisions)
+                    a[key], collisions = self.meld(b[key],
+                                                   a[key],
+                                                   path + [str(key)],
+                                                   collisions)
                 elif a[key] == b[key]:
                     pass
                 else:
@@ -137,3 +173,27 @@ class Record(object):
             return (False, collisions)
         else:
             return (True, a)
+
+    def get_value_from_key_list(self, keyList, start=None):
+        if start is None:
+            start = self.get_dictionary()
+        nextKey = keyList.pop(0)
+        if len(keyList) == 0:
+            return(start, start[nextKey])
+        else:
+            assert(nextKey in start)
+            return self.navigate_key_list(keyList, start=start[nextKey])
+
+    def set_value_from_key_list(self, keyList, new_value, start=None):
+        if start is None:
+            start = self.get_dictionary()
+        nextKey = keyList.pop(0)
+        if len(keyList) == 0:
+            start[nextKey] = new_value
+            return True
+        else:
+            assert(nextKey in start)
+            self.navigate_key_list(keyList, new_value, start=start[nextKey])
+
+    def blank_value_from_key_list(self, keyList, start=None):
+        return set_value_from_key_list(keyList, "", start=start)
